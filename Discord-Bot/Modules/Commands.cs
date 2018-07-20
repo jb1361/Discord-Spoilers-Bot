b37ca0e8654d0c09893d3ec6a26ff4738ed8c156
jb1361 <justinbutler4@hotmail.com>
2018-07-19 20:40:52 -0400

    Create C# project. Create Bot.

---
+++ Discord-Bot/Modules/Commands.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace Discord_Bot.Modules
+{ 
+    public class Commands : ModuleBase<SocketCommandContext>  
+    {
+       //Run delete messages every 5 minutes
+        Timer timer = new Timer(300000);
+        
+           
+        [Command("help")]
+        public async Task HelpAsync()
+        {
+            await ReplyAsync("commands are !start, !stop, and !delete-messages");
+        }
+
+        [Command("start")]
+        public async Task StartAsync()
+        {
+            timer.Elapsed += async (sender, e) => await DeleteMessagesAsync();
+            timer.AutoReset = true;
+            timer.Start();
+            await ReplyAsync("I am now deleting messages that are over an hour old.");
+
+        }
+
+        [Command("stop")]
+        public async Task StopAsync()
+        {
+            timer.Stop();         
+            await ReplyAsync("I am no longer deleting messages.");
+        }
+
+
+        [Command("watch")]      
+        [RequireBotPermission(ChannelPermission.ManageMessages)]
+        public async Task DeleteMessagesAsync()
+        {
+            try
+            {
+                var messages = await Context.Channel.GetMessagesAsync().Flatten();
+                foreach (IUserMessage message in messages)
+                {
+                    var now = DateTime.Now.ToUniversalTime();
+                    var messageTimestamp = message.Timestamp.UtcDateTime;
+                    var elapsed = now - messageTimestamp;
+
+                    if (elapsed.Hours > 0) await message.DeleteAsync();
+                    //Can check if minutes are over 20.
+                    //if (elapsed.Minutes > 1) await message.DeleteAsync();
+                    await Task.Delay(5000);
+                }
+            }catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
---
+++ Discord-Bot/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System.Threading.Tasks;
+using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+using System.Timers;
+
+namespace Discord_Bot
+{
+    class Bot
+    {
+        static void Main(string[] args) => new Bot().Connect().GetAwaiter().GetResult();
+
+        private DiscordSocketClient _client;
+        private CommandService _commands;
+        private IServiceProvider _services;
+
+        
+        public async Task Connect()
+        {
+            _client = new DiscordSocketClient();
+            _commands = new CommandService();
+    
+            _services = new ServiceCollection()
+            .AddSingleton(_client)
+            .AddSingleton(_commands)
+            .BuildServiceProvider();
+
+            _client.Log += Log;
+
+         
+            await RegisterCommandsAsync();
+
+            await _client.LoginAsync(TokenType.Bot, "Insert-Bot-Client-ID");         
+
+            await _client.StartAsync();      
+
+            await Task.Delay(-1);
+
+           
+
+
+        }
+
+     
+        private Task Log(LogMessage arg)
+        {
+            Console.WriteLine(arg);
+            return Task.CompletedTask;
+        }
+
+        public async Task RegisterCommandsAsync()
+        {
+            _client.MessageReceived += HandleCommandAsync;
+
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly());          
+        }
+
+        private async Task HandleCommandAsync(SocketMessage arg)
+        {
+            var message = arg as SocketUserMessage;
+
+            if (message is null || message.Author.IsBot) return;
+
+            int argPos = 0;
+
+            if (message.HasStringPrefix("!", ref argPos) || message.HasMentionPrefix(_client.CurrentUser, ref argPos))
+            {
+                var context = new SocketCommandContext(_client, message);
+
+                var result = await _commands.ExecuteAsync(context, argPos, _services);
+
+                if (!result.IsSuccess) Console.WriteLine(result.ErrorReason);
+            }
+        }
+    }//Class
+}
